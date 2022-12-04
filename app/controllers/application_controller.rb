67e0ea9ea5102905d06b49e00e922626fe9e54b6
EC2 Default User <dd.flash85@gmail.com>
2022-12-04 10:35:14 +0000

    [edit]views controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 新規登録時の項目カスタム対応
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name, :description, :url])
+  end
 end
